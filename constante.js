bf2c257ca6afeb8685e6254b2eff1e287e9ab46f
Chivu Marius <chivumarius@yahoo.com>
2021-06-28 15:25:56 +0300

    Constantele

---
+++ constante.js
@@ -0,0 +1,16 @@
+/* ==========================================
+    CONSTANTE
+=============================================*/
+
+/*
+    NB!
+    'CONSTANTELE' - SUNT 'VARIABILE' ALE CAROR 'VALORI' RAMAN 'NESCHIMBATE'
+*/
+
+// 'DECLARAREA' & 'INITIALIZAREA' - 'CONSTANTEI'
+const rataDobanzi = 0.3;
+
+
+
+// AFISARE
+console.log(rataDobanzi);
