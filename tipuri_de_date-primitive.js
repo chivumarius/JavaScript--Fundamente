38844fc6e0d272492ce43e14622b030bdc57d176
Chivu Marius <chivumarius@yahoo.com>
2021-06-28 16:01:14 +0300

    Tipuri de Date in JS

---
+++ tipuri_de_date-primitive.js
@@ -0,0 +1,46 @@
+/* ==========================================
+   TIPURI DE DATE (PRIMITIVE)
+=============================================*/
+
+/*
+    __________________________________________________
+    IN 'JS' EXISTA 2 CATEGORI DE TIPURI DE DATE color: 'rgba(255, 255, 255, 1)',
+       [I]  TIPURI 'PRIMITIVE'
+       [II] TIPURI DE 'REFERINTA'
+    __________________________________________________
+    
+    __________________________________________________
+    TIPURI 'PRIMITIVE' color: 'rgba(255, 255, 255, 1)',
+       (1) STRING
+       (2) NUMBER
+       (3) BOOLEAN
+       (4) UNDEFINED
+       (5) NUL
+    __________________________________________________
+*/
+
+// ==========================================
+// (1) 'STRING'  ('SIR LITERAL')
+let nume = 'Marius';
+
+// ==========================================
+// (2) 'NUMBER' ('NUMAR LITERAL')
+let varsta = 30;
+
+// ==========================================
+// (3) 'BOOLEAN' ('BOLEAN LITERAL')
+let esteAprobat = true;
+let esteAprobat = false;
+
+// ==========================================
+// (4) 'UNDEFINED' ('NEDEFINIT') -
+//     (APARE CAND 'VARIABILA' NU ESTE 'INITIALIZATA')
+let primulNume;
+
+// POATE FI 'SETATA EXPLICIT' CA SI 'NEDEFINITA'
+let primulNume = undefined;
+
+// ==========================================
+// (5) 'NULL' 
+// (ESTE 'UTILIZAT' CAND SE 'DORESTE STERGEREA VALORI' UNEI 'VATIABILE')
+let culoareaSelectata = null;
