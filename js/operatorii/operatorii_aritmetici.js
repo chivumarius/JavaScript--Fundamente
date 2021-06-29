86947413ad4de67754c8347312a6ed246d9895e3
Chivu Marius <chivumarius@yahoo.com>
2021-06-29 18:52:54 +0300

    Operatorii 'Aritmetici

---
+++ js/operatorii/operatorii_aritmetici.js
@@ -0,0 +1,60 @@
+/* ==========================================
+    OPERATORII  'ARITMETICI'
+=============================================*/
+
+/*
+    __________________________________________________
+    * Utilizam 'Operatori'  impreuna  cu  'Variabile'  & 'Constantele'
+            ==> pentru a crea Expresii
+            ==> iar cu acestea pute Implementa 'Logici' & 'Algoritmi.'
+    * UTILIZAM 'OPERATORII  ARITMETICI' => PT. A REALIZA 'CALCULE' 
+    
+    * 'EXPRESIILE' => SUNT 'INSTRUCTIUNI' CE 'PRODUC' O 'VALOARE.'
+    __________________________________________________
+*/
+
+let x = 10;
+let y = 5;
+
+// =============================================
+// (1) OP. DE  'ADAUGARE' (EXPRESIE):
+console.log(x + y);
+
+// =============================================
+// (2) OP. DE  'MULTIPLICARE' (EXPRESIE):
+console.log(x * y);
+
+// =============================================
+// (3) OP.  DE  'DIVIZARE' (EXPRESIE):
+console.log(x / y);
+
+// =============================================
+// (4) OP.  DE  'RESTURI'/'REMAINDERUP' (EXPRESIE):
+console.log(x % y);
+
+// =============================================
+// (5) OP.  DE  'EXPONENTIERE'/'EXPONENTIATION' SAU
+//              'RIDICARE LA PATRAT' (EXPRESIE):
+console.log(x ** y);
+
+// =============================================
+// (6) OP.  DE  'INCREMENTARE' (++):
+
+// FARA 'INCREMENTARE':
+// console.log(x);
+
+// (6.1) OP. DE 'PRE-INCREMENTARE'
+//       ('VALOAREA' = ESTE 'CRESCUTA' & APOI 'AFISATA')
+// console.log(++x);
+
+// (6.2) OP. DE 'POST-INCREMENTARE'
+// (SE AFISEAZA 'VALOAREA' LUI 'X' & IAR APOI 'VALOAREA' LUI 'X' ESTE 'CRESCUTA' CU '1')
+// console.log(x++);
+
+// REAFISAM  FARA 'INCREMENTARE':
+// console.log(x);
+
+// =============================================
+// (7) OP.  DE  'DECREMENTARE' (--):
+//  ('VALOAREA' LUI 'X' ESTE 'SCAZUTA' CU '1' & APOI 'AFISATA')
+console.log(--x);
