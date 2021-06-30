c3a95f4d5c3be5f0d8c646f0af8bb0f9fb462d12
Chivu Marius <chivumarius@yahoo.com>
2021-06-30 09:16:27 +0300

    Operatorii de 'Comparatie'

---
+++ js/operatorii/operatorii_de_comparatie.js
@@ -0,0 +1,38 @@
+/* ==========================================
+    OPERATORII  DE  'COMPARATIE'
+=============================================*/
+
+/*
+    __________________________________________________
+    * Utilizam 'Operatori de Comparatie'  impreuna  cu  'Variabile'  & 'Constantele'
+            ==> pentru a Compara 'Valoare' unei 'Variabile'
+            ==> cu 'Altceva'. 
+    * Rezultatul unei 'Comparatii' ==> este 'boolean' (Adevarat sau Fals).  
+     __________________________________________________
+*/
+
+let x = 1;
+
+// =============================================
+// (1.1) OP. 'RELATIONAL' - 'MAI MARE DECAT' ('>'):
+console.log(x > 0);
+
+// =============================================
+// (1.2) OP. 'RELATIONAL' - 'MAI MARE DECAT' SAU 'EGAL CU' ('<='):
+console.log(x >= 1);
+
+// =============================================
+// (1.3) OP. 'RELATIONAL' - 'MAI MIC DECAT' ('<'):
+console.log(x < 1);
+
+// =============================================
+// (1.4) OP. 'RELATIONAL' - 'MAI MIC DECAT' SAU 'EGAL CU' ('<='):
+console.log(x <= 1);
+
+// =============================================
+// (2.1) OP. DE 'EGALITATE' - 'EGAL CU' ('==='):
+console.log(x === 1);
+
+// =============================================
+// (2.2) OP. DE 'EGALITATE' - 'DIFERIT DE' ('!=='):
+console.log(x !== 1);
