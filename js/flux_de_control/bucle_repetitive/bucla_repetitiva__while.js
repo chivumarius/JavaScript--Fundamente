14eadee724dbc38020902bdf6264692085a959d3
Chivu Marius <chivumarius@yahoo.com>
2021-07-01 11:59:19 +0300

    Bucla Repetitiva 'while'

---
+++ js/flux_de_control/bucle_repetitive/bucla_repetitiva__while.js
@@ -0,0 +1,61 @@
+/* ============================================================================
+        FLUX  DE  CONTROL - 
+        BUCLA  REPETITIVA -- 'WHILE'
+===============================================================================*/
+/*
+    __________________________________________________________________________
+    (NB!) PT. 'REPETAREA' UNEI 'ACTIUNI', PT. UN ANUMIT 'NR. DE ORI' 
+            => SE UTILIZEAZA 'BUCLE REPETITIVE' ('LOOPS').
+    
+    IN JAVASCRIPT EXISTA URMAT. 'TIPURI'  DE  'BUCLE REPETITIVE'color: 'rgba(255, 255, 255, 1)',
+        (1) BUCLA 'FOR'
+        (2) BUCLA 'WHILE'
+        (3) BUCLA 'DO ... WHILE'
+        (4) BUCLA 'FOR ... IN'
+        (5) BUCLA 'FOR ... OF'
+    __________________________________________________________________________
+*/
+
+/*
+    __________________________________________________________________________
+    BUCLA REPETITIVA - 'WHILE'
+
+    SINTAXA:
+
+        WHILE (CONDITIA) {
+                ..INSTRUCTIUNI
+            }
+    __________________________________________________________________________
+*/
+
+
+
+//===============================================================================
+// BUCLA 'FOR' CU 'CONDITIONALA':
+//===============================================================================
+// for (let i = 0; i <= 5; i++) {
+
+// CONDITIONALA (AFLARE 'NR. IMPARe'):
+//     if (i % 2 !== 0)
+//         console.log(i);
+// }
+
+
+//===============================================================================
+//  BUCLA  REPETITIVA 'WHILE' 
+//===============================================================================
+
+// INITIALIZREA  VARIABILEI:
+let i = 0;
+
+// BUCLA 'WHILE':
+while (i <= 5) {
+    // INSTRUCTIUNEA 'CONDITIONALA' (AFLARE 'NR. IMPARe'):
+    if (i % 2 !== 0) {
+        // AFISAREA 'I':
+        console.log(i);
+    }
+
+    // INCREMENTAREA:
+    i++;
+}
