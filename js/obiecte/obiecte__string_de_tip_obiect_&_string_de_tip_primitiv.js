936fa1eb5128531b544b692a673783ca531150ac
Chivu Marius <chivumarius@yahoo.com>
2021-07-05 18:09:08 +0300

    Obiecte - 'String' de Tip 'Obiect' & 'String' de Tip 'Primitiv'

---
+++ js/obiecte/obiecte__string_de_tip_obiect_&_string_de_tip_primitiv.js
@@ -0,0 +1,27 @@
+/* =============================================================
+   OBIECTE:
+   'STRING' DE TIP 'OBIECT' & 'STRING' DE TIP 'PRIMITIV'
+============================================================= */
+
+/*
+    (*) STRING-URILE IN JAVASCRIPT SUNT DE 2 TIPURI:
+        => STRING DE TIP 'PRIMITIV'
+        => STRING DE TIP 'OBIECT'
+           
+    (*) 'GHILIMELELE' => POT FI ADAUGATE IN 'STRING'
+        => PRIN 'BACKSLASH' \' 
+           
+    (*) 'LINIEA NOUA' => POATE FI ADAUGATE IN 'STRING'
+        => PRIN 'BACKSLASH + N' (\n) 
+*/
+
+//=============================================================
+//  (1) STRING 'PRIMITIV'
+//=============================================================
+const mesaj = 'Acesta este primul meu mesaj!';
+
+
+//=============================================================
+//  (2) STRING 'OBIECT' - FUNC. CONSTRUCTOR 'STRING()'
+//=============================================================
+const altul = new String('Va saluta Marius!');
