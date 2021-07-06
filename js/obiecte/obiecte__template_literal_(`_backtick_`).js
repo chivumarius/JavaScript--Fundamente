6d9bf797c4a141116dc3daf16043afe835c3ed58
Chivu Marius <chivumarius@yahoo.com>
2021-07-06 14:28:46 +0300

    Obiecte - Template Literal (`Apostof/Backtick`)

---
+++ js/obiecte/obiecte__template_literal_(`_backtick_`).js
@@ -0,0 +1,26 @@
+/* =============================================================
+   OBIECTE:
+   'TEMPLATE LITERAL' (`APOSTROF/BACKTICK`)
+============================================================= */
+
+/*
+    (*) APOSTROFUL /BACKTICK `` 
+            => PERMITE CREAREA DE STRING-URI PERSONALIZATE
+            => FARA A SE UTILIZA 'CONCATENARI' SAU
+            => ESCAPE CHARACTERS (\N)
+           
+    (*) IN STRING -> PUTEM UTILIZA --> 'PLACEHOLDER' (${...cod})
+*/
+
+
+//=============================================================
+//  TEMPATE LITERAL
+//=============================================================
+const nume = 'Cristian';
+const mesaj =
+    `Salut ${nume},
+
+Iti multumesc pentru aderarea la grupul nostru!
+
+Cu stina,
+Narius Chivu`;
